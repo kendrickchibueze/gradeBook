417133f3888a96365982542ff185e21c72207bcd
kendrick chukwuka <kendrickchibueze@gmail.com>
2023-01-10 01:33:08 +0100

    Add project files.

---
+++ test/GradeBook.Test/BookTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GradeBook;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace GradeBook.Test
+{
+    [TestClass()]
+    public class BookTests
+    {
+        [Fact]
+        public void BookCalculatesAnAverageGrade()
+        {
+            // arrange
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+
+            // act
+            var result = book.GetStatistics();
+
+            // assert
+            Assert.Equal(85.6, result.Average, 1);
+            Assert.Equal(90.5, result.High, 1);
+            Assert.Equal(77.3, result.Low, 1);
+            Assert.Equal('B', result.Letter);
+        }
+
+    }
+}
